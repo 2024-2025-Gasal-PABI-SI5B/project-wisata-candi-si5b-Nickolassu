9dae18d43409bcc6a4cf23b5428a2756f458de88
Nickolassu <nickolassu_2226240047@mhs.mdp.ac.id>
2024-11-14 08:11:14 +0700

    part 3 (not done)

---
+++ wisata_candi/lib/screens/profile_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget{
+  const ProfileScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
+class _ProfileScreenState extends State
